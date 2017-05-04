2ddbe9310ad6df37be3ceed0b202785de48f1ceb
RoBareiro <rsb.bareiro@live.com.ar>
2017-05-03 23:31:02 -0300

    Commit que faltaba de getters y setters
    Lo que no me tomo el anterior commit

--- Farmaclick/src/main/java/ar/edu/unlam/tallerweb1/modelo/Usuario.java
+++ Farmaclick/src/main/java/ar/edu/unlam/tallerweb1/modelo/Usuario.java
@@ -9,16 +9,33 @@ import javax.persistence.Id;
 public class Usuario {
 	
 	@Id 
-	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	private String nombre;
+	private String apellido;
 	private String email;
 	private String password;
+	
+
+	/*GETTERS & SETTERS*/	
 	
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
 	}
 	public String getEmail() {
 		return email;
@@ -32,4 +49,5 @@ public class Usuario {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
 }
