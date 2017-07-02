cf20d02ceab4ee7e5d94ab5a2c5dc66f8f1550a7
martinezmsms <martinezmsms@gmail.com>
2017-07-02 13:23:32 -0300

    Mock test en FarmaciaTest

--- src/test/java/ar/edu/unlam/tallerweb1/TestFarmacia.java
+++ src/test/java/ar/edu/unlam/tallerweb1/TestFarmacia.java
@@ -4,11 +4,18 @@ import javax.inject.Inject;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.ModelAndView;
 
+import static org.mockito.Mockito.*;
+
+import ar.edu.unlam.tallerweb1.controladores.ControladorRegistroFarmacia;
 import ar.edu.unlam.tallerweb1.dao.FarmaciaDao;
 import ar.edu.unlam.tallerweb1.modelo.Farmacia;
+import ar.edu.unlam.tallerweb1.servicios.ServicioFarmacia;
 
 public class TestFarmacia extends SpringTest {
 	
@@ -55,20 +62,23 @@ public class TestFarmacia extends SpringTest {
 		Farmacia farmaciaObtenida = sessionFactory.getCurrentSession().get(Farmacia.class, farmaciaBuscada.getId());
 		Assert.assertTrue(farmaciaObtenida.getId().equals(farmaciaBuscada.getId()));
 	}
-//	
-//	@Test
-//	public void TestQuePruebaFarmaciaCorrecta(){
-//		ControladorRegistroFarmacia farmaciaControlador = new ControladorRegistroFarmacia();
-//		ServicioFarmacia farmaFake=mock(ServicioFarmacia.class);
-//		farmaciaControlador.setServicioFarmacia(farmaFake);
-//		
-//		Farmacia miFarmacia=new Farmacia();
-//		Farmacia farmaciaObtenida=new Farmacia();
-//		when(farmaFake.logear(miFarmacia)).thenReturn(farmaciaObtenida);
-//		
-//		ModelAndView mav = farmaciaControlador.validarLoginFarmacia(miFarmacia);
-//		
-//		assertThat(mav.getViewName()).isEqualTo("home");
-//		
-//	}
+	
+	@Test
+	public void TestQuePruebaFarmaciaCorrecta(){
+		ControladorRegistroFarmacia farmaciaControlador = new ControladorRegistroFarmacia();
+		ServicioFarmacia farmaFake = mock(ServicioFarmacia.class);
+		farmaciaControlador.setServicioFarmacia(farmaFake);
+		
+		Farmacia miFarmacia = new Farmacia();
+		Farmacia farmaciaObtenida = new Farmacia();
+		when(farmaFake.logear(miFarmacia)).thenReturn(farmaciaObtenida);
+		
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		
+		ModelAndView mav = farmaciaControlador.validarLoginFarmacia(request, response, miFarmacia);
+		
+		Assert.assertEquals("home", mav.getViewName());
+		
+	}
 }
